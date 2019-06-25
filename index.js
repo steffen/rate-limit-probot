a8c92365b2a602f246f03b0e862a0a89b1497a5f
Steffen Hiller <steffen@github.com>
2019-06-25 07:59:55 +0200

    Change /rate_limit to /debug and add getting installation_id support

--- index.js
+++ index.js
@@ -12,11 +12,25 @@ module.exports = app => {
     return context.github.issues.createComment(issueComment)
   })
 
-  commands(app, 'rate_limit', async (context, command) => {
-    const rateLimit = await context.github.rateLimit.get()
+  commands(app, 'debug', async (context, command) => {
+    let issueComment
 
-    const issueComment = context.issue({ body: '```\n' + JSON.stringify(rateLimit.data, null, 2) + '\n```' })
-    return context.github.issues.createComment(issueComment)
+    switch (command.arguments) {
+      case 'rate_limit':
+        const rateLimit = await context.github.rateLimit.get()
+
+        issueComment = context.issue({ body: '```\n' + JSON.stringify(rateLimit.data, null, 2) + '\n```' })
+        await context.github.issues.createComment(issueComment)
+        break
+      case 'installation_id':
+        const installationId = context.payload.installation.id
+
+        issueComment = context.issue({ body: `Installation ID: **${installationId}**` })
+        await context.github.issues.createComment(issueComment)
+        break
+    }
+
+    return
   })
 
   // For more information on building apps:
